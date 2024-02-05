7f5ebc1006effb6fd259943309c064a7742a04c6
Juanita Velez <jvalentina.velez@gmail.com>
2024-02-04 22:24:45 -0500

    directiva *ngIf

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+  public name: string = 'Ironman';
+  public age: number = 45;
+
+  get capitalizedName() :string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription() :string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this.name = 'Batman';
+  }
+
+  changeAge(): void {
+    this.age = 50;
+  }
+
+  resetForm(): void{
+    this.name = 'Ironman';
+    this.age = 45; 
+  }
+    
+}
